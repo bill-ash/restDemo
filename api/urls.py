c124f482a6f699008207deac6e9fb99c6bb0c17a
bill ash <wrash0806@my.mwsu.edu>
2021-03-06 21:34:45 -0500

    Refactor class based view by inheriting APIView.

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views 
+from api.views import (
+    TodoList, TodoDetail
+)
 
 app_name = 'api'
 
 urlpatterns = [
-    path('todo/', views.todo_list, name='todo_list'),
-    path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
+    # path('todo/', views.todo_list, name='todo_list'),
+    # path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
+    path('todo/', TodoList.as_view(), name='todo_list'),
+    path('todo/<int:pk>', TodoDetail.as_view(), name='todo_detail'),
 
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt 
 
 from rest_framework.parsers import JSONParser 
@@ -7,6 +7,7 @@ from rest_framework import status
 # For function based views 
 from rest_framework.decorators import api_view
 # For class based views
+from rest_framework.views import APIView
 # For propely handling content negotiation of response objects 
 # Make sure the response content is what the client requested 
 from rest_framework.response import Response 
@@ -14,8 +15,65 @@ from rest_framework.response import Response
 # Format suffix patterns: format=None adds support to handle an array of content 
 # types since our Response objects will now default to whatever the clinet requests. 
 
+# TodoSerializer requires a *data* argument. data=request.data 
+
 from .models import Todo
 from .serializers import TodoSerializer
+
+# Class based views inherit from APIView
+class TodoList(APIView): 
+
+    def get(self, request, format=None): 
+        queryset = Todo.objects.all()
+        todos = TodoSerializer(queryset, many=True)
+        return Response(todos.data)
+        
+    def post(self, request, format=None): 
+        todo = TodoSerializer(data=request.data)
+        if todo.is_valid(): 
+            todo.save()
+            return Response(todo.data, status=status.HTTP_201_CREATED)
+        return Response(todo.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class TodoDetail(APIView): 
+
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None): 
+        queryset = self.get_object(pk)
+        todo = TodoSerializer(queryset)
+        return Response(todo.data)
+
+    def put(self, request, pk, format=None): 
+        queryset = self.get_object(pk)
+        todo = TodoSerializer(queryset, data=request.data)
+        if todo.is_valid(): 
+            todo.save()
+            return Response(todo.data)
+        return Response(todo.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk, format=None): 
+        queryset = self.get_object(pk)
+        todo = TodoSerializer(queryset, data=request.data)
+        if todo.is_valid(): 
+            todo.save()
+            return Response(todo.data)
+        return Response(todo.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        queryset = self.get_object(pk)
+        # No need to serialize - delete 
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
 
 # Decorator adds features to request object so they can be handled by serializers. 
 @api_view(['GET', 'POST'])
@@ -50,7 +108,9 @@ def todo_detail(request, pk, format=None):
         return Response(serializer.data)
 
     elif request.method == 'PUT': 
-        todo = TodoSerializer(request.data)
+        # Serializer takes two arguments, the queryset in the database, 
+        # and the incoming request object.
+        todo = TodoSerializer(queryset, data = request.data)
         if todo.is_valid(): 
             todo.save()
             return Response(todo.data)
