2a8456835c6bde7ac061708d02f407d6e02c4b9b
bill ash <wrash0806@my.mwsu.edu>
2021-03-06 20:46:06 -0500

    Refactor using rest_frame work functions Response(), and decorators for adding serialization attirbutes to requests.

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from . import views 
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views 
 
 app_name = 'api'
 
 urlpatterns = [
     path('todo/', views.todo_list, name='todo_list'),
     path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
-    
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+    
--- api/views.py
+++ api/views.py
@@ -1,53 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt 
+
 from rest_framework.parsers import JSONParser 
+from rest_framework import status 
+# For function based views 
+from rest_framework.decorators import api_view
+# For class based views
+# For propely handling content negotiation of response objects 
+# Make sure the response content is what the client requested 
+from rest_framework.response import Response 
+
+# Format suffix patterns: format=None adds support to handle an array of content 
+# types since our Response objects will now default to whatever the clinet requests. 
 
 from .models import Todo
 from .serializers import TodoSerializer
 
-@csrf_exempt
-def todo_list(request): 
+# Decorator adds features to request object so they can be handled by serializers. 
+@api_view(['GET', 'POST'])
+def todo_list(request, format=None): 
+
     if request.method == 'GET': 
         queryset = Todo.objects.all()
         # many=True for more than one object (querysets)
         serializer = TodoSerializer(queryset, many=True)
         # I think there is another way to do this 
         # return HttpResponse(serializer.data, content='application/json')
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TodoSerializer(data)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid(): 
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.error, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def todo_detail(request, pk): 
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail(request, pk, format=None): 
 
     try: 
-        queryset = Todo.objects.get(id=pk)
+        queryset = Todo.objects.get(pk=pk)
     except Todo.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
         
     if request.method == 'GET':
         serializer = TodoSerializer(queryset)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'POST': 
-        data = JSONParser(request)
-        todo = TodoSerializer(data)
+    elif request.method == 'PUT': 
+        todo = TodoSerializer(request.data)
         if todo.is_valid(): 
             todo.save()
-            return JsonResponse(todo.data, status=201)
+            return Response(todo.data)
         else: 
-            return JsonResponse(todo.error, status=400)
+            return Response(todo.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    else:
-        return JsonResponse({'error': 'Could not process'})
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
     
